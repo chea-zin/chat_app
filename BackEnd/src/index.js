8acdc4cd3a78f103e86e868060f5a002f628ace9
@chea99 <chea02310@gmail.com>
2025-05-03 15:48:14 +0700

    som change

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT
 app.use("/api/auth", authRoutes)
 app.listen(5001, ()=>{
-    console.log("Server is running on port 5001");
+    console.log("Server is running on port" + PORT);
 })
